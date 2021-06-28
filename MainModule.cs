66ea45b75d96c4895bd50944ac63210a6c50769e
Jacob Persi <me@JacobPersi.com>
2021-06-28 12:22:20 -0400

    Initial commit

---
+++ MainModule.cs
@@ -0,0 +1,19 @@
+﻿using TaleWorlds.InputSystem;
+using TaleWorlds.MountAndBlade;
+
+public class MainModule : MBSubModuleBase
+{
+    protected override void OnSubModuleLoad()
+    {
+    }
+
+    protected override void OnApplicationTick(float dt)
+    {
+        /* Hacky way of loading our scene */
+        if (Input.IsKeyDown(InputKey.F1))
+        {
+            MBGameManager.StartNewGame((MBGameManager)new CustomGameManager());
+        }
+    }
+
+}
