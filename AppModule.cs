35d1ef2a4417be8ecec925d59e87a001ed747a72
Jacob Persi <me@JacobPersi.com>
2022-08-08 19:41:49 -0400

    Update AppModule.cs

--- AppModule.cs
+++ AppModule.cs
@@ -5,6 +5,8 @@ namespace CustomMission {
     public class AppModule : MBSubModuleBase {
 
         public override void OnInitialState() {
+            // This project assumes you want to create your own game outside of the standard campaign environment. 
+            // To do this, we create a custom MBGameManager called CustomGameManager...
             MBGameManager.StartNewGame((MBGameManager)new CustomGameManager());
         }
     }
